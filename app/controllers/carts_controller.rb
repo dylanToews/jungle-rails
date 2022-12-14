29cf534c6d32ab18dd33465fb8747251bf0aa033
dylanToews <dylan.toews@yahoo.ca>
2022-12-14 09:39:43 -0700

    money formatting bug fixed money formatting bug in multiple locations to display money in correct format

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,20 @@
 class CartsController < ApplicationController
 
   def show
+    # @product = Product.find(params[:id])
+  # if cart is empty, display alert. 
+    # if product_id
+    #   empty_cart_alert
+    # end
   end
 
+  def empty_cart_alert
+
+      puts "hello"
+
+  end
+  
+  
   def add_item
     product_id = params[:product_id].to_s
     modify_cart_delta(product_id, +1)
