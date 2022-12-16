a1451d5872d630754d152b1076391a0eca0cbdb5
dylanToews <dylan.toews@yahoo.ca>
2022-12-16 12:36:26 -0700

    user authentification feature new feature added for user to create a new user, login and logout

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
 
+
+
   def show
       if cart.present?
         render "show"
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
   namespace :admin do
     get 'categories/index'
   end
   get 'about/index'
   
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   
   root to: 'products#index'
   
